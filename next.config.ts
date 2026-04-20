d04bda50a4347dc8b03cef8af65bd95c215d9010
Jnsgt <15832812380@163.com>
2026-04-20 21:32:18 +0800

    更新部署

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+module.exports = {
+  basePath: '/zenviorxpage', // 如果部署在 GitHub Pages 的子路径下，修改为你的仓库名称
+  assetPrefix: '/zenviorxpage/', // 配置静态资源路径
+  trailingSlash: true, // 确保 URL 以斜杠结尾，适配 GitHub Pages 路由
+  // 其他配置...
 };
-
-export default nextConfig;
