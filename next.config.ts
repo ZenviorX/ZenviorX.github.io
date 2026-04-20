c8ecdf478196f7ee100934166f45648d1b03e628
Jnsgt <15832812380@163.com>
2026-04-20 23:21:50 +0800

    更新环境

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 module.exports = {
+  exportTrailingSlash: true,
   basePath: '/zenviorxpage', // 如果部署在 GitHub Pages 的子路径下，修改为你的仓库名称
   assetPrefix: '/zenviorxpage/', // 配置静态资源路径
   trailingSlash: true, // 确保 URL 以斜杠结尾，适配 GitHub Pages 路由
