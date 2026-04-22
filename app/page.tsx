d213bccc853a8d652273b3b2a7ec0e9ac7847c55
Jnsgt <15832812380@163.com>
2026-04-22 21:15:35 +0800

    更新 page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -93,7 +93,7 @@ export default function Home() {
             </div>
 
             <div className="mt-10 rounded-[28px] border border-white/70 bg-white/58 p-5 shadow-[0_14px_36px_rgba(15,23,42,0.06)] md:p-7">
-              <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-4">
+              <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
                 {quickStats.map((item) => (
                   <div
                     key={item.label}
